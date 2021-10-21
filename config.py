01088403bce034fd3e500f6b21dbe47d17448379
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-22 05:05:31 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+
+# For Local Deploy
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+# Necessary Vars
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SESSION = os.getenv("SESSION")
+HNDLR = os.getenv("HNDLR", "!")
+
+
+contact_filter = filters.create(
+    lambda _, __, message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Vcmusic"))
+call_py = PyTgCalls(bot)
