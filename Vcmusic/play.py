f77967b390ef5f595e6d72413da84bca75cfa41a
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-22 12:44:30 +0700

    Update play.py

--- Vcmusic/play.py
+++ Vcmusic/play.py
@@ -12,6 +12,12 @@ from pytgcalls.types.input_stream import AudioPiped
 from youtubesearchpython import VideosSearch
 
 
+async def eor(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+
 def ytsearch(query):
    try:
       search = VideosSearch(query, limit=1)
