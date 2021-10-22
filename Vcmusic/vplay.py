b9cb667e23404e40a218d35a5a52db9f5ae3b505
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-22 15:51:12 +0700

    Update vplay.py

--- Vcmusic/vplay.py
+++ Vcmusic/vplay.py
@@ -32,7 +32,7 @@ def ytsearch(query):
 # https://github.com/pytgcalls/pytgcalls/blob/dev/example/youtube_dl/youtube_dl_example.py
 async def ytdl(link):
    proc = await asyncio.create_subprocess_exec(
-       'youtube-dl',
+       'yt-dlp',
        '-g',
        '-f',
        # CHANGE THIS BASED ON WHAT YOU WANT
@@ -150,7 +150,7 @@ async def vplay(client, m: Message):
                else:
                   if chat_id in QUEUE:
                      pos = add_to_queue(chat_id, songname, ytlink, url, "Video", Q)
-                     await huehue.edit(f"🔢 Video added at position » **#{pos}**")
+                     await huehue.edit(f"🔢 Video added at position » **{pos}**")
                   else:
                      try:
                         await call_py.join_group_call(
