0fac99c5a6892b2d3e1e65852b933bb423fdf8aa
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-22 15:52:30 +0700

    lol

--- Vcmusic/__init__.py
+++
@@ -1,6 +0,0 @@
-from pyrogram.types import Message
-
-async def eor(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    return await func(**{k: v for k, v in kwargs.items() if k in spec})
