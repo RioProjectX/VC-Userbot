f3739e3005d50b3c1cbfe51af32018579b8d5374
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-23 21:17:30 +0700

    Update ping.py

--- Vcmusic/ping.py
+++ Vcmusic/ping.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
+from config import bot, call_py, HNDLR, OWNER_ID
 from time import time
 from time import sleep
 from datetime import datetime
@@ -30,11 +30,11 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-@Client.on_message(contact_filter & filters.command(['ping'], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.user(OWNER_ID) & filters.command(['ping'], prefixes=f"{HNDLR}"))
 async def ping(client, m: Message):
    start = time()
    current_time = datetime.utcnow()
-   m_reply = await m.reply_text("`Pinging...`")
+   m_reply = await m.edit("`Pinging...`")
    delta_ping = time() - start
    await m_reply.edit("**0% ▒▒▒▒▒▒▒▒▒▒**")
    await m_reply.edit("**20% ██▒▒▒▒▒▒▒▒**")
@@ -48,11 +48,11 @@ async def ping(client, m: Message):
    uptime = await _human_time_duration(int(uptime_sec))
    await m_reply.edit(f"🏓 `Pong!!`\n**Speed -** `{delta_ping * 1000:.3f} ms` \n**Uptime** - `{uptime}`")
 
-@Client.on_message(contact_filter & filters.command(['pink'], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.user(OWNER_ID) & filters.command(['pink'], prefixes=f"{HNDLR}"))
 async def pong(client, m: Message):
    start = time()
    current_time = datetime.utcnow()
-   pong = await m.reply_text("`KONTOL...`")
+   pong = await m.edit("`KONTOL...`")
    delta_ping = time() - start
    await pong.edit("8✊===D")
    await pong.edit("8=✊==D")
@@ -79,12 +79,12 @@ async def pong(client, m: Message):
    await pong.edit(
        f"**KONTOL!! **\n✨ **NGENTOT** : {delta_ping * 1000:.3f} ms\n**⏱️ Bot Uptime** : {uptime}")
 
-@Client.on_message(contact_filter & filters.command(['help'], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.user(OWNER_ID) & filters.command(['help'], prefixes=f"{HNDLR}"))
 async def help(client, m: Message):
-   HELP = f"**💡 Help menu** \n\n__👥 All Users command__ (Anyone can Use): \n• `{HNDLR}play` \n• `{HNDLR}vplay` \n• `{HNDLR}stream` (For Radio links) \n• `{HNDLR}vstream` (For .m3u8 / live links) \n• `{HNDLR}playfrom [channel] ; [n]` - Plays last n songs from channel \n• `{HNDLR}playlist` / `{HNDLR}queue` \n• `{HNDLR}repo \n\n__👮 Sudo users command__ (Can only be accessed by You and Your Contacts): \n• `{HNDLR}ping` \n• `{HNDLR}skip` \n• `{HNDLR}pause` and `{HNDLR}resume` \n• `{HNDLR}stop` / `{HNDLR}end` \n• `{HNDLR}help`"
-   await m.reply(HELP)
+   HELP = f"**💡 Help menu** \n\n📚 __All Commands__ : \n• `{HNDLR}play` \n• `{HNDLR}vplay` \n• `{HNDLR}stream` (For Radio links) \n• `{HNDLR}vstream` (For .m3u8 / live links) \n• `{HNDLR}playfrom [channel] ; [n]` - Plays last n songs from channel \n• `{HNDLR}playlist` / `{HNDLR}queue` \n• `{HNDLR}repo \n• `{HNDLR}ping` \n• `{HNDLR}skip` \n• `{HNDLR}pause` and `{HNDLR}resume` \n• `{HNDLR}stop` / `{HNDLR}end` \n• `{HNDLR}help`\n\n📝 Notes: Your contacts is sudo users, can control your userbot musicplayer"
+   await m.edit(HELP)
 
-@Client.on_message(filters.command(['repo'], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.user(OWNER_ID) & filters.command(['repo'], prefixes=f"{HNDLR}"))
 async def repo(client, m: Message):
    KONTOL = f"⚙️ **Source code**\n• **😼 Github :** [Vcmusic-Userbot](https://github.com/KennedyProject/Vcmusic-Userbot)\n• 🗂️ **GPL - 3.0 License**"
-   await m.reply(KONTOL, disable_web_page_preview=True)
+   await m.edit(KONTOL, disable_web_page_preview=True)
