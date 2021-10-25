f07be0d5ac14a4123fa918f41fc5dda0218164ae
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-25 11:53:53 +0700

    Update stickers.py

--- Vcmusic/stickers.py
+++ Vcmusic/stickers.py
@@ -44,8 +44,7 @@ async def kang_(client, message: Message):
             await message.edit("`Unsupported File!`")
             return
         await message.edit(f"`{random.choice(KANGING_STR)}`")
-        photo = await client.download_media(message=replied,
-                                            file_name=DOWN_PATH)
+        photo = await client.download_media(file_name=DOWN_PATH)
     else:
         await message.err("`I can't kang that...`")
         return
