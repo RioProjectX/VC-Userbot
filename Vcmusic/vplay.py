e8d42216008065c5d577ee1bf642b5a5d19e3262
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-26 19:28:36 +0700

    Update vplay.py

--- Vcmusic/vplay.py
+++ Vcmusic/vplay.py
@@ -48,7 +48,7 @@ async def ytdl(link):
       return 0, stderr.decode()
 
 
-@Client.on_message(contact_filter & filters.command(['vplay'], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(['vplay'], prefixes=f"{HNDLR}"))
 async def vplay(client, m: Message):
    replied = m.reply_to_message
    chat_id = m.chat.id
@@ -168,7 +168,7 @@ async def vplay(client, m: Message):
                         await huehue.edit(f"`{ep}`")
 
 
-@Client.on_message(contact_filter & filters.command(['vstream'], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(['vstream'], prefixes=f"{HNDLR}"))
 async def vstream(client, m: Message):
    chat_id = m.chat.id
    if len(m.command) < 2:
