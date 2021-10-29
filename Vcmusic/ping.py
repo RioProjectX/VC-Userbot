44bfeff9db448ca5b7e5957bf08e6ea02646066b
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-29 08:30:55 +0700

    Update ping.py

--- Vcmusic/ping.py
+++ Vcmusic/ping.py
@@ -85,5 +85,5 @@ async def help(client, m: Message):
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command(['repo'], prefixes=f"{HNDLR}"))
 async def repo(client, m: Message):
-   KONTOL = f"⚙️ **Source code**\n• **😼 Github :** [Vcmusic-Userbot](https://github.com/KennedyProject/Vcmusic-Userbot)\n• 🗂️ **GPL - 3.0 License**"
+   KONTOL = f"⚙️ **Source code**\n• **😼 Github :** [VC-Userbot](https://github.com/rioprojectx/Vc-Userbot)\n• 🗂️ **GPL - 3.0 License**"
    await m.edit(KONTOL, disable_web_page_preview=True)
