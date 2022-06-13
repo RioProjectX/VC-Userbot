e07166b6f394686086c5b956f90c8cbeddc6983f
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2022-06-13 22:53:58 +0700

    Update play.py

--- MusicRioUserbot/play.py
+++ MusicRioUserbot/play.py
@@ -14,7 +14,7 @@ from pytgcalls.types.input_stream.quality import (
 from youtubesearchpython import VideosSearch
 
 from config import HNDLR, bot, call_py
-from MusicUserbot.helpers.queues import QUEUE, add_to_queue, get_queue
+from MusicRioUserbot.helpers.queues import QUEUE, add_to_queue, get_queue
 
 AMBILFOTO = [
     "https://telegra.ph/file/81cb39831b7fa309b56bb.jpg",
