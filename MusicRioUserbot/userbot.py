e595d7cdb69c47c35f564843c519f3da1ffd1c8a
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2022-06-13 12:53:45 +0700

    1

---
+++ MusicRioUserbot/userbot.py
@@ -0,0 +1,144 @@
+import os
+import sys
+from datetime import datetime
+from time import time
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import HNDLR, SUDO_USERS
+
+# System Uptime
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ("Minggu", 60 * 60 * 24 * 7),
+    ("Hari", 60 * 60 * 24),
+    ("Jam", 60 * 60),
+    ("Menit", 60),
+    ("Detik", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else ""))
+    return ", ".join(parts)
+
+@Client.on_message(filters.command(["ping"], prefixes=f"{HNDLR}"))
+async def ping(client, m: Message):
+   start = time()
+   current_time = datetime.utcnow()
+   m_reply = await m.edit("Pinging ngewe...")
+   delta_ping = time() - start
+   await m_reply.edit("0% ▒▒▒▒▒▒▒▒▒▒")
+   await m_reply.edit("20% ██▒▒▒▒▒▒▒▒")
+   await m_reply.edit("40% ████▒▒▒▒▒▒")
+   await m_reply.edit("60% ██████▒▒▒▒")
+   await m_reply.edit("80% ████████▒▒")
+   await m_reply.edit("100% ██████████")
+   end = datetime.now()
+   uptime_sec = (current_time - START_TIME).total_seconds()
+   uptime = await _human_time_duration(int(uptime_sec))
+   await m_reply.edit(f"**┞◈𝗣𝗼𝗻𝗴!!🏓**\n**┞◈Pinger**  - {delta_ping * 1000:.3f} ms \n**┞◈Uptime** - {uptime}")
+
+
+@Client.on_message(filters.command(["pong"], prefixes=f"{HNDLR}"))
+async def pong(client, m: Message):
+   start = time()
+   current_time = datetime.utcnow()
+   pong = await m.edit("kontol jamet...")
+   delta_ping = time() - start
+   await pong.edit("❏◈===❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏===◈❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏◈===❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏===◈❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏◈===❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏===◈❏")
+   await pong.edit("❏===◈❏◈")
+   await pong.edit("❏====❏◈◈")
+   await pong.edit("**◈PINGGGG!**")
+   end = datetime.now()
+   uptime_sec = (current_time - START_TIME).total_seconds()
+   uptime = await _human_time_duration(int(uptime_sec))
+   await pong.edit(
+       f"**❏RioMusicUserbot**\n**❏Pinging** : {delta_ping * 1000:.3f} ms\n**❏Bot Uptime** : {uptime}")
+
+@Client.on_message(
+    filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=f"{HNDLR}")
+)
+async def restart(client, m: Message):
+    await m.delete()
+    loli = await m.reply("1")
+    await loli.edit("2")
+    await loli.edit("3")
+    await loli.edit("4")
+    await loli.edit("5")
+    await loli.edit("6")
+    await loli.edit("7")
+    await loli.edit("8")
+    await loli.edit("9")
+    await loli.edit("**✅ Music Userbot Di Restart Ulang**")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
+
+
+@Client.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+async def help(client, m: Message):
+    await m.delete()
+    HELP = f"""
+<b>👋 Hallo {m.from_user.mention}!
+🛠 MENU BANTUAN MUSIC PLAYER
+⚡ PERINTAH UNTUK SEMUA ORANG
+• {HNDLR}play [judul lagu | link youtube | balas file audio] - untuk memutar lagu
+• {HNDLR}videoplay [judul video | link youtube | balas file video] - untuk memutar video
+• {HNDLR}playlist untuk melihat daftar putar
+• {HNDLR}ping - untuk cek status
+• {HNDLR}id - untuk melihat id pengguna
+• {HNDLR}video - judul video | link yt untuk mencari video
+• {HNDLR}song - judul lagu | link yt untuk mencari lagu
+• {HNDLR}help - untuk melihat daftar perintah
+• {HNDLR}join- untuk join | ke grup 
+⚡ PERINTAH UNTUK SEMUA ADMIN
+• {HNDLR}resume - untuk melanjutkan pemutaran lagu atau video
+• {HNDLR}pause - untuk untuk menjeda pemutaran lagu atau video
+• {HNDLR}skip - untuk melewati lagu atau video
+• {HNDLR}end - untuk mengakhiri pemutaran</b>
+"""
+    await m.reply(HELP)
+
+
+@Client.on_message(filters.command(["repo"], prefixes=f"{HNDLR}"))
+async def repo(client, m: Message):
+    await m.delete()
+    REPO = f"""
+<b>👋 Hallo {m.from_user.mention}!
+🗃️ Music Dan Video Player UserBot
+🔰 Telegram UserBot Untuk Memutar Lagu Dan Video Di Obrolan Suara Telegram.
+👩‍💻 Maintaned By 
+• [Rio](https://t.me/fckualot)
+📝 Persyaratan
+• Python 3.8+
+• FFMPEG
+• Nodejs v16+
+[Repo MusicUserbot](https://github.com/Zaen-ubot/MusicUserbot)
+📝 Variabel Yang Dibutuhkan
+• `API_ID` - Dapatkan Dari [my.telegram.org](https://my.telegram.org)
+• `API_HASH` - Dapatkan Dari [my.telegram.org](https://my.telegram.org)
+• `SESSION` - Sesi String Pyrogram.
+• `SUDO_USER` - ID Akun Telegram Yang Digunakan Sebagai Admin
+• `HNDLR` - Handler untuk menjalankan userbot mu
+"""
+    await m.reply(REPO, disable_web_page_preview=True)
